f28ee94176f2561515bfbddbff1d9fffdc5a4d88
jlaura <jlaura@asu.edu>
2017-12-01 08:46:27 -0700

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,8 @@
+set LIBRARY_BIN=%ANACONDA%\Library\bin
+set LIBRARY_INC=%ANACONDA%\Library\include
+set LIBRARY_LIB=%ANADONDA%\Library\lib
+set "PATH=%LIBRARY_BIN%;%LIBRARY_INC%;%LIBRARY_LIB%:%PATH%"
+
 python setup.py install
 
 if errorlevel 1 exit 1
