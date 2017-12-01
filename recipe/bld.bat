c96f818197a4cf9597ba40eb488a04bc24681217
jlaura <jlaura@asu.edu>
2017-12-01 08:48:04 -0700

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 :: Update the PATH to include the Anaconda Library
-set "PATH=%LIBRARY_BIN%;%LIBRARY_INC%;%LIBRARY_LIB%:%PATH%"
+set "PATH=%LIBRARY_BIN%;%LIBRARY_INC%;%LIBRARY_LIB%;%PATH%"
 
 python setup.py install
 
