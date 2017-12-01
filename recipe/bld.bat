d24fb9cbaf1c59ec18aa92f2cfb4d2374dd93cfa
jlaura <jlaura@asu.edu>
2017-12-01 11:20:46 -0700

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,8 @@
-python setup.py build_ext --include-dirs %LIBRARY_INC% --library-dirs %LIBRARY_LIB%
+setenv /x64 / release
+set INCLUDE=%INCLUDE%;%LIBRARY_INC%
+set LIB=%LIB%;%LIBRARY_BIN%;%LIBRARY_LIB%
+set DISTUTILS_USE_SDK=1
+
 python setup.py install
 
 if errorlevel 1 exit 1
