c6520fb02ec9a703e750653e6309dbde1ea13a93
jay <jlaura@asu.edu>
2017-12-05 14:31:42 -0700

    App

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-python setup.py build_ext --include-dirs %LIBRARY_INC% -- library-dirs %LIBRARY_LIB%;%LIBRARY_BIN%
+python setup.py build --compiler=msvc --include-dirs %LIBRARY_INC% -- library-dirs %LIBRARY_LIB%;%LIBRARY_BIN%
 python setup.py install
 
 if errorlevel 1 exit 1
