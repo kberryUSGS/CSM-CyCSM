bbd7fc8c50b43cb426e1ccec903ed7d09da26600
jlaura <jlaura@asu.edu>
2017-12-06 07:46:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,10 +18,9 @@ COMPILE_ARGS = ['-g', '-std=c++11'] #, '-std=c++11']
 if sys.platform == 'darwin':
     COMPILE_ARGS.append('-mmacosx-version-min=10.9')
 elif sys.platform.startswith("win"):
-    print('WIN')
-    print(os.environ['PATH'])
     try:
         INCLUDE_DIRS.append(os.environ['LIBRARY_INC'])
+        INCLUDE_DIRS.append(os.path.join(os.environ['LIBRARY_INC'], 'csm'))
         print(os.environ['LIBRARY_INC'])
     except: pass
     try:
